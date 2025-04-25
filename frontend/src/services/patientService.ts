2962a66d96420c96898b4d7acf981b7158ee8bed
Igor <igorcostalins2005@gmail.com>
2025-04-25 16:23:11 -0300

    feat: Create patient service for be consumed

---
+++ frontend/src/services/patientService.ts
@@ -0,0 +1,48 @@
+import { defaultErrorFunction } from '../utils/errorTratament'
+import { Patient } from '../interfaces/patient'
+
+
+export async function getPatients(cpf: string = ''): Promise<Patient | null> {
+    try {
+        const response = await fetch(`http://localhost:8000/api/v1/patient/${cpf}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+              },
+        })
+    
+        if (!response.ok) {
+            throw new Error('Bad return from get patient')
+        }
+        
+        const dataPatient : Patient = await response.json()
+        return dataPatient || null
+
+    } catch (error) {
+        defaultErrorFunction(error)
+        return null
+    }
+}
+
+
+export async function postPatient(patient: Patient): Promise<boolean> {
+    try {
+        const response = await fetch(`http://localhost:8000/api/v1/patient/`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(patient)
+        })
+
+        if (!response.ok) {
+            throw new Error('Bad return from get patient')
+        }
+
+        return true
+
+    } catch (error) {
+        defaultErrorFunction(error)
+        return false
+    }
+}
