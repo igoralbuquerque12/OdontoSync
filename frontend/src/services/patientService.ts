2f0a43a958241dae1caa11eb5dc5e8a7c94b10fb
Igor <igorcostalins2005@gmail.com>
2025-06-09 22:53:11 -0300

    refactore: refactor return of service functions

--- frontend/src/services/patientService.ts
+++ frontend/src/services/patientService.ts
@@ -1,8 +1,12 @@
 import { defaultErrorFunction } from '../utils/errorTratament'
 import { Patient } from '../interfaces/patient'
 
+interface returnType {
+    status: boolean
+    content: Patient | string | null
+}
 
-export async function getPatients(cpf: string = ''): Promise<Patient | null> {
+export async function getPatient(cpf: string = ''): Promise<returnType> {
     try {
         const response = await fetch(`http://localhost:8000/api/v1/patient/${cpf}`, {
             method: 'GET',
@@ -16,16 +20,22 @@ export async function getPatients(cpf: string = ''): Promise<Patient | null> {
         }
         
         const dataPatient : Patient = await response.json()
-        return dataPatient || null
+        return {
+            status: true,
+            content: dataPatient
+        }
 
     } catch (error) {
         defaultErrorFunction(error)
-        return null
+        return { 
+            status: false,
+            content: 'Não foi possível buscar os pacientes cadastrados.'
+        }
     }
 }
 
 
-export async function postPatient(patient: Patient): Promise<boolean> {
+export async function postPatient(patient: Patient): Promise<returnType> {
     try {
         const response = await fetch(`http://localhost:8000/api/v1/patient/`, {
             method: "POST",
@@ -36,13 +46,22 @@ export async function postPatient(patient: Patient): Promise<boolean> {
         })
 
         if (!response.ok) {
-            throw new Error("Bad return from get patient")
+            const erroData = await response.json()
+            if (erroData.cpf) {
+                return { status: false, content: 'Já existe um paciente cadastrado com este CPF.' }
+            } 
+
+            throw new Error('Houve um erro desconhecido.')
         }
 
-        return true
+        const nameSplit = patient.name.split(' ')
+        return { status: true, content: `Paciente ${nameSplit[0]} ${nameSplit[nameSplit.length - 1]} cadastrado com sucesso.` }
 
     } catch (error) {
         defaultErrorFunction(error)
-        return false
+        return {
+            status: false,
+            content: 'Houve um erro desconhecido.'
+        }
     }
 }
